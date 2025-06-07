dd79eaf84966fffe4c723ff88f41e2afee646903
Lucas12Magalhaes <lucas12magalhaes@hotmail.com>
2025-06-07 18:43:22 -0300

    Alt: Layout telaInicial e telaLogin

--- sistema-autonomo/Telas/1 - Login.Designer.cs
+++ sistema-autonomo/Telas/1 - Login.Designer.cs
@@ -29,8 +29,10 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnListarPartidas = new System.Windows.Forms.Button();
-            this.lstPartidas = new System.Windows.Forms.ListBox();
             this.lstJogadores = new System.Windows.Forms.ListBox();
             this.lblVersaoJogo = new System.Windows.Forms.Label();
             this.cboFiltrarPartidas = new System.Windows.Forms.ComboBox();
@@ -40,6 +42,11 @@
             this.btnCriarPartida = new System.Windows.Forms.Button();
             this.txtInputSenhaPartida = new System.Windows.Forms.TextBox();
             this.txtInputNomePartida = new System.Windows.Forms.TextBox();
+            this.dgvPartidas = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NOME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.STATUS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPartidas)).BeginInit();
             this.SuspendLayout();
             // 
             // btnListarPartidas
@@ -50,32 +57,28 @@
             this.btnListarPartidas.FlatAppearance.BorderSize = 0;
             this.btnListarPartidas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnListarPartidas.ForeColor = System.Drawing.Color.Transparent;
-            this.btnListarPartidas.Location = new System.Drawing.Point(933, 250);
+            this.btnListarPartidas.Location = new System.Drawing.Point(933, 241);
             this.btnListarPartidas.Name = "btnListarPartidas";
             this.btnListarPartidas.Size = new System.Drawing.Size(122, 22);
             this.btnListarPartidas.TabIndex = 0;
             this.btnListarPartidas.UseVisualStyleBackColor = false;
             this.btnListarPartidas.Click += new System.EventHandler(this.btnListarPartidas_Click);
             // 
-            // lstPartidas
-            // 
-            this.lstPartidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lstPartidas.FormattingEnabled = true;
-            this.lstPartidas.ItemHeight = 24;
-            this.lstPartidas.Location = new System.Drawing.Point(767, 288);
-            this.lstPartidas.Name = "lstPartidas";
-            this.lstPartidas.Size = new System.Drawing.Size(311, 100);
-            this.lstPartidas.TabIndex = 2;
-            this.lstPartidas.SelectedIndexChanged += new System.EventHandler(this.lstPartidas_SelectedIndexChanged);
-            // 
             // lstJogadores
             // 
-            this.lstJogadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstJogadores.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstJogadores.BackColor = System.Drawing.Color.White;
+            this.lstJogadores.CausesValidation = false;
+            this.lstJogadores.Font = new System.Drawing.Font("Baskerville Old Face", 14.25F, System.Drawing.FontStyle.Bold);
             this.lstJogadores.FormattingEnabled = true;
-            this.lstJogadores.ItemHeight = 24;
-            this.lstJogadores.Location = new System.Drawing.Point(794, 431);
+            this.lstJogadores.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lstJogadores.ItemHeight = 22;
+            this.lstJogadores.Location = new System.Drawing.Point(776, 423);
             this.lstJogadores.Name = "lstJogadores";
-            this.lstJogadores.Size = new System.Drawing.Size(262, 76);
+            this.lstJogadores.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.lstJogadores.Size = new System.Drawing.Size(289, 92);
             this.lstJogadores.TabIndex = 14;
             // 
             // lblVersaoJogo
@@ -91,7 +94,7 @@
             this.cboFiltrarPartidas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboFiltrarPartidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cboFiltrarPartidas.FormattingEnabled = true;
-            this.cboFiltrarPartidas.Location = new System.Drawing.Point(786, 245);
+            this.cboFiltrarPartidas.Location = new System.Drawing.Point(786, 236);
             this.cboFiltrarPartidas.Name = "cboFiltrarPartidas";
             this.cboFiltrarPartidas.Size = new System.Drawing.Size(121, 32);
             this.cboFiltrarPartidas.TabIndex = 16;
@@ -139,7 +142,7 @@
             this.btnCriarPartida.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCriarPartida.FlatAppearance.BorderSize = 0;
             this.btnCriarPartida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCriarPartida.Location = new System.Drawing.Point(286, 415);
+            this.btnCriarPartida.Location = new System.Drawing.Point(281, 403);
             this.btnCriarPartida.Name = "btnCriarPartida";
             this.btnCriarPartida.Size = new System.Drawing.Size(118, 36);
             this.btnCriarPartida.TabIndex = 23;
@@ -167,6 +170,87 @@
             this.txtInputNomePartida.Size = new System.Drawing.Size(137, 31);
             this.txtInputNomePartida.TabIndex = 2;
             // 
+            // dgvPartidas
+            // 
+            this.dgvPartidas.AllowUserToAddRows = false;
+            this.dgvPartidas.AllowUserToDeleteRows = false;
+            this.dgvPartidas.AllowUserToResizeColumns = false;
+            this.dgvPartidas.AllowUserToResizeRows = false;
+            this.dgvPartidas.BackgroundColor = System.Drawing.Color.White;
+            this.dgvPartidas.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvPartidas.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgvPartidas.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPartidas.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvPartidas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPartidas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.NOME,
+            this.STATUS});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Baskerville Old Face", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvPartidas.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvPartidas.GridColor = System.Drawing.Color.White;
+            this.dgvPartidas.Location = new System.Drawing.Point(744, 283);
+            this.dgvPartidas.MultiSelect = false;
+            this.dgvPartidas.Name = "dgvPartidas";
+            this.dgvPartidas.ReadOnly = true;
+            this.dgvPartidas.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Info;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            this.dgvPartidas.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvPartidas.RowHeadersVisible = false;
+            this.dgvPartidas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPartidas.ShowCellErrors = false;
+            this.dgvPartidas.ShowCellToolTips = false;
+            this.dgvPartidas.Size = new System.Drawing.Size(362, 110);
+            this.dgvPartidas.TabIndex = 28;
+            this.dgvPartidas.SelectionChanged += new System.EventHandler(this.dgvPartidas_SelectionChanged);
+            // 
+            // ID
+            // 
+            this.ID.Frozen = true;
+            this.ID.HeaderText = "ID";
+            this.ID.MaxInputLength = 5;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.ID.Width = 50;
+            // 
+            // NOME
+            // 
+            this.NOME.Frozen = true;
+            this.NOME.HeaderText = "NOME";
+            this.NOME.MaxInputLength = 15;
+            this.NOME.Name = "NOME";
+            this.NOME.ReadOnly = true;
+            this.NOME.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.NOME.Width = 215;
+            // 
+            // STATUS
+            // 
+            this.STATUS.Frozen = true;
+            this.STATUS.HeaderText = "STATUS";
+            this.STATUS.MaxInputLength = 10;
+            this.STATUS.Name = "STATUS";
+            this.STATUS.ReadOnly = true;
+            this.STATUS.Width = 80;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -174,9 +258,9 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.dgvPartidas);
             this.Controls.Add(this.btnEntrarPartida);
             this.Controls.Add(this.btnListarPartidas);
-            this.Controls.Add(this.lstPartidas);
             this.Controls.Add(this.lstJogadores);
             this.Controls.Add(this.txtSenhaEntrarPartida);
             this.Controls.Add(this.txtInputNomePartida);
@@ -193,6 +277,7 @@
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "KingMe";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPartidas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,7 +286,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnListarPartidas;
-        private System.Windows.Forms.ListBox lstPartidas;
         private System.Windows.Forms.ListBox lstJogadores;
         private System.Windows.Forms.Label lblVersaoJogo;
         private System.Windows.Forms.ComboBox cboFiltrarPartidas;
@@ -211,5 +295,9 @@
         private System.Windows.Forms.Button btnCriarPartida;
         private System.Windows.Forms.TextBox txtInputSenhaPartida;
         private System.Windows.Forms.TextBox txtInputNomePartida;
+        private System.Windows.Forms.DataGridView dgvPartidas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NOME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn STATUS;
     }
 }
